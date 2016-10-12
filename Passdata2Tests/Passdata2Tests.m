fd607b2d01b3c4f421df08fa4c6598436f90d7b3
MadhuriJagtap <madhuri@felix-its.com>
2016-10-12 10:24:59 +0530

    Initial Commit

---
+++ Passdata2Tests/Passdata2Tests.m
@@ -0,0 +1,39 @@
+//
+//  Passdata2Tests.m
+//  Passdata2Tests
+//
+//  Created by Felix-Madhuri on 12/10/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Passdata2Tests : XCTestCase
+
+@end
+
+@implementation Passdata2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
